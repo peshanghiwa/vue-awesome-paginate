dd9d0f21e7a4bf8e8e9096c9bfbaa1b4809f51ca
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-07-22 22:37:51 +0300

    configured typescript declaration for build time

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "src/package.ts"),
       name: "vue-awesome-paginate",
       fileName: (format) => `vue-awesome-paginate.${format}.js`,
     },
     rollupOptions: {
       external: ["vue"],
+
       output: {
         globals: {
           vue: "Vue",
@@ -20,5 +22,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [vue()],
+  plugins: [typescript(), vue()],
 });
