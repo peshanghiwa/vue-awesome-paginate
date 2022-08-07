3d727ec9fa668da11fdb89a3ed1e6c0a4c1b0132
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-08-07 03:39:59 +0300

    new vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
 import { defineConfig } from "vite";
+
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
-import typescript from "@rollup/plugin-typescript";
+import typescript2 from "rollup-plugin-typescript2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    typescript2({
+      check: false,
+      include: ["src/components/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+        exclude: ["vite.config.ts", "main.ts"],
+      },
+    }),
+  ],
   build: {
+    cssCodeSplit: false,
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "vue-awesome-paginate",
-      fileName: (format) => `vue-awesome-paginate.${format}.js`,
+      entry: "./src/VueAwesomePaginatePlugin.ts",
+      formats: ["es", "cjs"],
+      name: "VueAwesomePaginate",
+      fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
     },
     rollupOptions: {
       external: ["vue"],
-
       output: {
         globals: {
           vue: "Vue",
@@ -21,6 +37,4 @@ export default defineConfig({
       },
     },
   },
-
-  plugins: [typescript(), vue()],
 });
